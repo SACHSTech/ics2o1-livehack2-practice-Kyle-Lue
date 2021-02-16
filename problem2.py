8cfddb88c2e261f78664b19bbaf3e47aabdd09b4
Jeffrey Lue <jeffrey.lue23@ycdsbk12.ca>
2021-02-16 13:20:38 +0000

    .

--- problem2.py
+++ problem2.py
@@ -1,1 +1,11 @@
+# get marks, earnings
+marks = float(input("Enter your mark: "))
+earnings = float(input("Enter your earnings: "))
 
+# compute and show destination
+if marks >= 80 and earnings >= 500:
+    print("You get to go to Europe.")
+elif marks >= 80:
+    print("You get to go to California.")
+else:
+    print("You stay home :(")
